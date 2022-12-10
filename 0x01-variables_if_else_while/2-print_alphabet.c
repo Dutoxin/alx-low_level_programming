10eeef758182e3ac839138816c18b469841d542a
Dutoxin <zlatanyacob@gmail.com>
2022-12-10 06:01:32 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: A program that prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		return (0);
+}
