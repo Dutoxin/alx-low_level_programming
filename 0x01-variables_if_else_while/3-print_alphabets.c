605f6eb0443f0e56eefbc4b0434751583d038289
Dutoxin <zlatanyacob@gmail.com>
2022-12-12 12:44:36 -0800

    Alphabet corrected

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  * Description: Print the alphabet in lowercase, and then in uppercase
- * Return; Always 0
+ * Return; Always 0 (Success)
  */
 int main(void)
 {
@@ -17,7 +17,6 @@ int main(void)
 		{
 			putchar(c);
 			c++;
-		
 		}
 	while
 		(d <= 'Z')
