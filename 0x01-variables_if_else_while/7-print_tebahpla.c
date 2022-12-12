712847718831a8f9b9dca16da258e428f41cbbdd
Dutoxin <zlatanyacob@gmail.com>
2022-12-11 23:28:42 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: a program that prints the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
