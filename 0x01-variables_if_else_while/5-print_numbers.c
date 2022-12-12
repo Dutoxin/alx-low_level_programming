e29bdb3265c96dd835b74907c0197a5401bbf194
Dutoxin <zlatanyacob@gmail.com>
2022-12-12 10:17:55 -0800

    Numbers corrected

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- * Description: A program that prints all single digit numbers of base 10 starting from 0, followed by a new line
+ * Description: Program that prints all single digit numbers of base 10 from 0
  * Return: Always 0
  */
 int main(void)
 {
 	char c = '0';
+
 	while (c <= '9')
 	{
 		putchar(c);
