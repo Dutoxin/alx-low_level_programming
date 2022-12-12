ded24783bf5d57bbb4e859d06794e62ac7393c0c
Dutoxin <zlatanyacob@gmail.com>
2022-12-12 10:27:56 -0800

    Numberz corrected again

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: a program that prints all single digit numbers of base 10 starting from 0, followed by a new line
+ * Description: Program that prints all single digit numbers of base 10 from 0
  * Return: Always 0
  */
 int main(void)
