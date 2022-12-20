97ebd34274ef8e7bffc85e6c5a13be02ed576b67
Dutoxin <zlatanyacob@gmail.com>
2022-12-20 00:10:58 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy - a finction that copies the string pointed to bu src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int I = 0;
+	int x = 0;
+
+	while (*(src + I) != '\0')
+	{
+		I++;
+	}
+	for ( ; x < I ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[I] = '\0';
+	return (dest);
+}
